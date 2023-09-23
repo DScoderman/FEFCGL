e0561ef07a66748edc506de0c0367056dd2e6664
DSCoderman <dsenour6@gmail.com>
2023-09-23 16:08:33 -0400

    added route structure, bootstrap and react-router-dom

---
+++ fantasy-football/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/layout';
+import HomePage from './pages/homePage/';
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element ={<HomePage />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ fantasy-football/src/pages/homePage/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomePage = () => {
+    return (
+        <div> 
+            Welcome to FEFCGL!
+        </div>
+
+    );
+}
+export default HomePage;
